17c0d26d4daa266d0da88c526504dbb29cf0c2ed
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-01-30 18:03:28 +0800

    Adds 0033 solution (python)

--- python/algorithms/0033.py
+++ python/algorithms/0033.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from functools import reduce
+from math import sqrt
+
 # :: ---
 
 def shares_digit (m, n):
@@ -26,10 +29,22 @@ def generator ():
             if result and (m / n == nm / nn):
                 yield m, n, nm, nn
 
+def lowest_terms (x, y):
+    n = 2
+    limit = int(sqrt(max(x, y)) + 0.5)
+
+    for d in range(2, limit + 1):
+        if x % n == 0 and y % n == 0:
+            return lowest_terms(x // n, y // n)
+        n += 1
+
+    return x, y
 
 def main ():
-    f = [ x for x in generator() ]
-    print(f)
+    f = [ (nm, nn) for m, n, nm, nn in generator() ]
+    numerator, denominator = reduce(lambda a, p: (a[0] * p[0], a[1] * p[1]), f)
+
+    print(lowest_terms(numerator, denominator))
 
 # :: ---
 if __name__ == "__main__":
