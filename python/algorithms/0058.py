f69aa9cc0282530382629b4578e12429734fb031
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-02-17 12:00:25 -0800

    Adds 0058 solution (python)

---
+++ python/algorithms/0058.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from math import sqrt
+from functools import lru_cache
+
+# :: ---
+
+@lru_cache(maxsize = None)
+def is_prime (n):
+    if n < 2:
+        return False
+    # :: ---
+    limit = int(sqrt(n) + 0.5)
+    for d in range(2, limit + 1):
+        if n % d == 0: return False
+
+    return True
+
+def generator ():
+    side_length = 1
+    candidate_count = 1
+    primes_count = 0
+
+    while True:
+        side_length += 2
+        base = side_length ** 2
+        d = side_length - 1
+        candidates = [ base - (x * d) for x in range(1, 4) ]
+        primes_count += sum(1 for x in candidates if is_prime(x))
+        candidate_count += 4
+
+        yield primes_count, candidate_count, side_length
+
+def main ():
+    g = generator()
+    print(next(s for (p, c, s) in g if (p / c < 0.10)))
+
+# :: ---
+if __name__ == "__main__":
+    main()
